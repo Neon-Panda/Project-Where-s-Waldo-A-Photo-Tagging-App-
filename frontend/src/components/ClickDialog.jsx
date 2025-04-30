84736c75dd58b43983e1ddf67015d398d830592e
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-04-30 04:09:09 -0500

    feat: make pop up after click prettier

--- frontend/src/components/ClickDialog.jsx
+++ frontend/src/components/ClickDialog.jsx
@@ -1,14 +1,25 @@
+import bottleCap from "../assets/bottle-cap.png";
+import dartBoard from "../assets/dart-board.png";
+import umbrella from "../assets/umbrella.png";
+import styles from "./ClickDialog.module.css";
+
 export default function ClickDialog({ positionCoords }) {
   console.log("inside click dialog");
   console.log(positionCoords);
   return (
-    <div style={{ position: "absolute", transform: `translate(${positionCoords.x}px, ${positionCoords.y}px)` }}>
-      <p>Example Dialong</p>
-      <form>
-        <button>Character 1</button>
-        <button>Character 2</button>
-        <button>Character 3</button>
-      </form>
-    </div>
+    <form
+      className={styles.dialogForm}
+      style={{ position: "absolute", transform: `translate(${positionCoords.x}px, ${positionCoords.y}px)` }}
+    >
+      <button className={styles.button}>
+        <img src={bottleCap} />
+      </button>
+      <button className={styles.button}>
+        <img src={dartBoard} />
+      </button>
+      <button className={styles.button}>
+        <img src={umbrella} />
+      </button>
+    </form>
   );
 }
